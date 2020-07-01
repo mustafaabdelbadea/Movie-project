947b67d69bf5bfed6df70c3f9c960fa3a3c99ab9
mustafa <mustafa.abdelbadea@gmail.com>
2020-07-01 16:26:17 +0200

    sign up form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -7,13 +7,15 @@ import { FormControl, FormGroup,Validators } from '@angular/forms'
 })
 export class SignupComponent implements OnInit {
   signupForm=new FormGroup({
-    'first_name':new FormControl(),
-    'last_name':new FormControl(),
-    'age':new FormControl(),
-    'email':new FormControl(),
-    'password':new FormControl(),
+    'first_name':new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
+    'last_name':new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
+    'age':new FormControl(null,[Validators.required,Validators.min(18),Validators.max(60)]),
+    'email':new FormControl(null,[Validators.required,Validators.email]),
+    'password':new FormControl(null,[Validators.required,Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)]),
   });
-
+  signUp(){
+        
+  }
   constructor() { }
 
   ngOnInit(): void {
